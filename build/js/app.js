0bcd4dcd388d50eda60a21f9aa1ddcad201a12a9
Aundra Miller and Mollie Bootsma <mollie.bootsma@gmail.com>
2016-03-15 15:41:34 -0700

    create routes and functions to calculate fahrenheit, celsius, and kelvin

--- build/js/app.js
+++ build/js/app.js
@@ -2,42 +2,102 @@
 exports.apiKey = "2791a6a4c12b08ce1a402af7119f7da2";
 
 },{}],2:[function(require,module,exports){
-function kelvinConverter(kelvin_input) {
+function kelvinConvertFahrenheit(kelvin_input) {
   var kelvin = parseInt(kelvin_input);
   var fahrenheit = (1.8 * (kelvin-273)) + 32;
   return Math.round(fahrenheit);
 };
 
+function kelvinConvertCelsius(kelvin_input) {
+  var kelvin = parseInt(kelvin_input);
+  var celsius = kelvin_input - 273.15;
+  return Math.round(celsius);
+};
 
+//Fahrenheit button
 $(document).ready(function() {
-  $("#weatherTemperature").click(function(event) {
+  $("#weatherTempFahrenheit").click(function(event) {
     event.preventDefault();
     var city = $("input#location").val();
     $.get("http://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + apiKey).then(function(response) {
-      $('.showWeather').text("The temperature in " + city + " is " + kelvinConverter(response.main.temp) + "°F")}).fail(function(error) {
+      $('.showWeather').text("The temperature in " + city + " is " + kelvinConvertFahrenheit(response.main.temp) + "°F")}).fail(function(error) {
         $('.showWeather').text(error.responseJSON.message);
       });
     });
   });
+
+//Celsius button
+  $(document).ready(function() {
+    $("#weatherTempCelsius").click(function(event) {
+      event.preventDefault();
+      var city = $("input#location").val();
+      $.get("http://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + apiKey).then(function(response) {
+        $('.showWeather').text("The temperature in " + city + " is " + kelvinConvertCelsius(response.main.temp) + "°C")}).fail(function(error) {
+          $('.showWeather').text(error.responseJSON.message);
+        });
+      });
+    });
+
+//Kelvin Button
+    $(document).ready(function() {
+      $("#weatherTempKelvin").click(function(event) {
+        event.preventDefault();
+        var city = $("input#location").val();
+        $.get("http://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + apiKey).then(function(response) {
+          $('.showWeather').text("The temperature in " + city + " is " + response.main.temp + "°K")}).fail(function(error) {
+            $('.showWeather').text(error.responseJSON.message);
+          });
+        });
+      });
 
 },{}],3:[function(require,module,exports){
-function kelvinConverter(kelvin_input) {
+function kelvinConvertFahrenheit(kelvin_input) {
   var kelvin = parseInt(kelvin_input);
   var fahrenheit = (1.8 * (kelvin-273)) + 32;
   return Math.round(fahrenheit);
 };
 
+function kelvinConvertCelsius(kelvin_input) {
+  var kelvin = parseInt(kelvin_input);
+  var celsius = kelvin_input - 273.15;
+  return Math.round(celsius);
+};
 
+//Fahrenheit button
 $(document).ready(function() {
-  $("#weatherTemperature").click(function(event) {
+  $("#weatherTempFahrenheit").click(function(event) {
     event.preventDefault();
     var city = $("input#location").val();
     $.get("http://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + apiKey).then(function(response) {
-      $('.showWeather').text("The temperature in " + city + " is " + kelvinConverter(response.main.temp) + "°F")}).fail(function(error) {
+      $('.showWeather').text("The temperature in " + city + " is " + kelvinConvertFahrenheit(response.main.temp) + "°F")}).fail(function(error) {
         $('.showWeather').text(error.responseJSON.message);
       });
     });
   });
+
+//Celsius button
+  $(document).ready(function() {
+    $("#weatherTempCelsius").click(function(event) {
+      event.preventDefault();
+      var city = $("input#location").val();
+      $.get("http://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + apiKey).then(function(response) {
+        $('.showWeather').text("The temperature in " + city + " is " + kelvinConvertCelsius(response.main.temp) + "°C")}).fail(function(error) {
+          $('.showWeather').text(error.responseJSON.message);
+        });
+      });
+    });
+
+//Kelvin Button
+    $(document).ready(function() {
+      $("#weatherTempKelvin").click(function(event) {
+        event.preventDefault();
+        var city = $("input#location").val();
+        $.get("http://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + apiKey).then(function(response) {
+          $('.showWeather').text("The temperature in " + city + " is " + response.main.temp + "°K")}).fail(function(error) {
+            $('.showWeather').text(error.responseJSON.message);
+          });
+        });
+      });
 
 var apiKey = require('./../.env').apiKey;
 var temperature = require('./../js/temperature-interface.js');
--- tmp/allConcat.js
+++ tmp/allConcat.js
@@ -1,20 +1,50 @@
-function kelvinConverter(kelvin_input) {
+function kelvinConvertFahrenheit(kelvin_input) {
   var kelvin = parseInt(kelvin_input);
   var fahrenheit = (1.8 * (kelvin-273)) + 32;
   return Math.round(fahrenheit);
 };
 
+function kelvinConvertCelsius(kelvin_input) {
+  var kelvin = parseInt(kelvin_input);
+  var celsius = kelvin_input - 273.15;
+  return Math.round(celsius);
+};
 
+//Fahrenheit button
 $(document).ready(function() {
-  $("#weatherTemperature").click(function(event) {
+  $("#weatherTempFahrenheit").click(function(event) {
     event.preventDefault();
     var city = $("input#location").val();
     $.get("http://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + apiKey).then(function(response) {
-      $('.showWeather').text("The temperature in " + city + " is " + kelvinConverter(response.main.temp) + "°F")}).fail(function(error) {
+      $('.showWeather').text("The temperature in " + city + " is " + kelvinConvertFahrenheit(response.main.temp) + "°F")}).fail(function(error) {
         $('.showWeather').text(error.responseJSON.message);
       });
     });
   });
+
+//Celsius button
+  $(document).ready(function() {
+    $("#weatherTempCelsius").click(function(event) {
+      event.preventDefault();
+      var city = $("input#location").val();
+      $.get("http://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + apiKey).then(function(response) {
+        $('.showWeather').text("The temperature in " + city + " is " + kelvinConvertCelsius(response.main.temp) + "°C")}).fail(function(error) {
+          $('.showWeather').text(error.responseJSON.message);
+        });
+      });
+    });
+
+//Kelvin Button
+    $(document).ready(function() {
+      $("#weatherTempKelvin").click(function(event) {
+        event.preventDefault();
+        var city = $("input#location").val();
+        $.get("http://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + apiKey).then(function(response) {
+          $('.showWeather').text("The temperature in " + city + " is " + response.main.temp + "°K")}).fail(function(error) {
+            $('.showWeather').text(error.responseJSON.message);
+          });
+        });
+      });
 
 var apiKey = require('./../.env').apiKey;
 var temperature = require('./../js/temperature-interface.js');
